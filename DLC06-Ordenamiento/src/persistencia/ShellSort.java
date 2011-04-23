a9b6201a600ab0be94202e12ae7dce5c6e396539
rodrigo.inf.liberal <rodrigo.inf.liberal@gmail.com>
2011-04-23 00:19:19 +0000

    ShellSort.java 100%

--- DLC06-Ordenamiento/src/persistencia/ShellSort.java
+++ DLC06-Ordenamiento/src/persistencia/ShellSort.java
@@ -19,7 +19,7 @@ public class ShellSort implements DirectAccessFileSorter
 
     public void sort() {
         // si algo anda mal, salir sin hacer nada...
-        if( rf == null || rf.getBaseClassName() == null || rf.getMode().equals( "r" ) ) return;
+        if( rf == null || rf.getBaseClassName() == null || rf.getMode().equals( "r" ) ) {System.out.println("Nada que ordenar");;return;}
 
         // obtenemos la cantidad de registros del archivo
         long n = rf.count();
@@ -29,25 +29,30 @@ public class ShellSort implements DirectAccessFileSorter
     }
     private void shell()
     {
-       long h,n=rf.length();
-       for(h = 1; h <= n / 9; h = 3*h + 1);
-       for ( ; h > 0; h /= 3)
-       {
-           for (long j = h; j < n; j++)
-             {
-                  Grabable y= rf.get(0);
-                  
-                  //for (k = j - h; k >= 0 && y < v[k]; k-=h)
-                  long k;
-                  for (k=j-h; k >= 0 && y.compareTo(rf.get(k))< 0; k-=h)
-                  {
-                        //v[k+h] = v[k];
-                        rf.set(k+h, rf.get(k));
-                  }
-                 //v[k+h] = y;
-                 rf.set(k+h, y);
-             }
+       long h;
+       long n=rf.count();
+       if(n==0){
+           System.out.println("Nada que ordenar");
        }
+       else{
+           for(h = 1; h <= n / 9; h = 3*h + 1);
+           for ( ; h > 0; h /= 3)
+           {
+               for (long j = h; j < n; j++)
+                 {
+                      Grabable y= rf.get(j);
 
+                      //for (k = j - h; k >= 0 && y < v[k]; k-=h)
+                      long k;
+                      for (k=j-h; k >= 0 && y.compareTo(rf.get(k))< 0; k-=h)
+                      {
+                            //v[k+h] = v[k];
+                            rf.set(k+h, rf.get(k));
+                      }
+                     //v[k+h] = y;
+                     rf.set(k+h, y);
+                 }
+           }
+       }
     }
 }
