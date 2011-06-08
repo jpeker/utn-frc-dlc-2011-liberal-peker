f983a4c6cc2bf0aa34ca4cd5f5cdb7e494cb1556
julianpeker <julianpeker@gmail.com>
2011-06-08 21:49:28 +0000

    script con tabla Comentarios

---
+++ clases practica/scripts-comentario/03.carga.sql
@@ -0,0 +1,44 @@
+-- =============================================================================
+-- USUARIOS
+-- =============================================================================
+-- Cargo usuario 1
+SELECT fn_getidusuario();
+SELECT fn_saveusuario(1, 'julian 01', 'peker 01', 'julian@hotmail.com');
+-- Cargo usuario 2
+SELECT fn_getidusuario();
+SELECT fn_saveusuario(2, 'rodrigo 02', 'liberal 02', NULL);
+--Cargo usuario 3
+SELECT fn_getidusuario();
+SELECT fn_saveusuario(3, 'juan 03','perez 03','juanperez@hotmail.com');
+-- Corrijo Dirección de usuario 1
+SELECT fn_saveusuario(1, 'julian 01', 'peker 01', 'julianpeker@hotmail.com');
+
+-- =============================================================================
+-- AMIGOS
+-- =============================================================================
+-- Cargo un amigo 1
+SELECT fn_saveamigo(1,2);
+-- Cargo un amigo 2
+SELECT fn_saveamigo(1,3);
+-- =============================================================================
+-- FOTO
+-- =============================================================================
+-- Cargo foto 1
+SELECT fn_getidfoto();
+SELECT fn_savefoto(1,'C:\\misdocumentos\fotobariloche.jpg',1,3,'20100120134510');
+-- Cargo foto 2 
+SELECT fn_getidfoto();
+SELECT fn_savefoto(2,'C:\\misdocumentos\fotoasado.jpg',1,3,'20110120134510');
+
+-- Intento ingresar una foto con un usuario sin correo de mail
+SELECT fn_savefoto(1,'C:\\misdocumentos\fotofiesta.jpg',1,2,'20110120134510');
+
+-- =============================================================================
+-- COMENTARIO
+-- =============================================================================
+-- Cargo foto 1
+SELECT fn_getidcomentario();
+SELECT fn_savecomentario(1,1,1,'livin');
+-- Cargo foto 2 
+SELECT fn_getidcomentario();
+SELECT fn_savecomentario(2,2,1,'tero');
