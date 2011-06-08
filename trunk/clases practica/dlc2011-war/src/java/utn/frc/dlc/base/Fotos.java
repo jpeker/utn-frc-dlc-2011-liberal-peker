367527de105af2d09f33e0b4f40d68f3a62e6852
julianpeker@gmail.com <julianpeker@gmail.com@1c33a77f-a4e5-06ae-344c-5a4c18e46920>
2011-06-08 21:47:33 +0000

    proyecto

---
+++ trunk/clases practica/dlc2011-war/src/java/utn/frc/dlc/base/Fotos.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utn.frc.dlc.base;
+
+/**
+ *
+ * @author dlcusr
+ */
+public class Fotos {
+
+
+    public Fotos() {
+    }
+    private String  pathfoto;
+    private int idAmigo1;
+    private int idAmigo2;
+    private int idfoto;
+
+    public int getIdAmigo1() {
+        return idAmigo1;
+    }
+
+    public void setIdAmigo1(int idAmigo1) {
+        this.idAmigo1 = idAmigo1;
+    }
+
+    public int getIdAmigo2() {
+        return idAmigo2;
+    }
+
+    public void setIdAmigo2(int idAmigo2) {
+        this.idAmigo2 = idAmigo2;
+    }
+
+    public int getIdfoto() {
+        return idfoto;
+    }
+
+    public void setIdfoto(int idfoto) {
+        this.idfoto = idfoto;
+    }
+
+    public String getPathfoto() {
+        return pathfoto;
+    }
+
+    public void setPathfoto(String pathfoto) {
+        this.pathfoto = pathfoto;
+    }
+
+
+}
