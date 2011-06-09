edc3e0559766cf52d44a1b4f501754a50ab2d276
julianpeker <julianpeker@gmail.com>
2011-06-09 23:53:56 +0000

    actualizacion de script

--- clases practica/scripts-comentario/03.carga.sql
+++ clases practica/scripts-comentario/03.carga.sql
@@ -6,7 +6,7 @@ SELECT fn_getidusuario();
 SELECT fn_saveusuario(1, 'julian 01', 'peker 01', 'julian@hotmail.com');
 -- Cargo usuario 2
 SELECT fn_getidusuario();
-SELECT fn_saveusuario(2, 'rodrigo 02', 'liberal 02', NULL);
+SELECT fn_saveusuario(2, 'rodrigo 02', 'liberal 02','rodrigo@hotmail.com');
 --Cargo usuario 3
 SELECT fn_getidusuario();
 SELECT fn_saveusuario(3, 'juan 03','perez 03','juanperez@hotmail.com');
@@ -29,16 +29,18 @@ SELECT fn_savefoto(1,'C:\\misdocumentos\fotobariloche.jpg',1,3,'20100120134510')
 -- Cargo foto 2 
 SELECT fn_getidfoto();
 SELECT fn_savefoto(2,'C:\\misdocumentos\fotoasado.jpg',1,3,'20110120134510');
-
+-- Cargo foto 3 
+SELECT fn_getidfoto();
+SELECT fn_savefoto(3,'prueba.jpg',1,2,'20110120134510');
 -- Intento ingresar una foto con un usuario sin correo de mail
-SELECT fn_savefoto(1,'C:\\misdocumentos\fotofiesta.jpg',1,2,'20110120134510');
+--SELECT fn_savefoto(1,'C:\\misdocumentos\fotofiesta.jpg',1,2,'20110120134510');
 
 -- =============================================================================
 -- COMENTARIO
 -- =============================================================================
--- Cargo foto 1
+-- Cargo com 1
 SELECT fn_getidcomentario();
 SELECT fn_savecomentario(1,1,1,'livin');
--- Cargo foto 2 
+-- Cargo com  2 
 SELECT fn_getidcomentario();
 SELECT fn_savecomentario(2,2,1,'tero');
