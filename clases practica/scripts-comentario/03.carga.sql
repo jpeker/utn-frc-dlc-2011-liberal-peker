142737d8dd6f87c57f16a08ca2d8e1d3312847a5
julianpeker <julianpeker@gmail.com>
2011-06-11 15:00:16 +0000

    tp con fotos probado anda bien solo vemos despues lo del boton

--- clases practica/scripts-comentario/03.carga.sql
+++ clases practica/scripts-comentario/03.carga.sql
@@ -3,15 +3,18 @@
 -- =============================================================================
 -- Cargo usuario 1
 SELECT fn_getidusuario();
-SELECT fn_saveusuario(1, 'julian 01', 'peker 01', 'julian@hotmail.com');
+SELECT fn_saveusuario(1, 'julian', 'peker', 'julian@hotmail.com');
 -- Cargo usuario 2
 SELECT fn_getidusuario();
-SELECT fn_saveusuario(2, 'rodrigo 02', 'liberal 02','rodrigo@hotmail.com');
+SELECT fn_saveusuario(2, 'rodrigo', 'liberal','rodrigo@hotmail.com');
 --Cargo usuario 3
 SELECT fn_getidusuario();
-SELECT fn_saveusuario(3, 'juan 03','perez 03','juanperez@hotmail.com');
+SELECT fn_saveusuario(3, 'juan','perez','juanperez@hotmail.com');
+--Cargo usuario 4
+SELECT fn_getidusuario();
+SELECT fn_saveusuario(4, 'gianna','ramos','gianna_cba@hotmail.com');
 -- Corrijo Dirección de usuario 1
-SELECT fn_saveusuario(1, 'julian 01', 'peker 01', 'julianpeker@hotmail.com');
+SELECT fn_saveusuario(1, 'julian', 'peker', 'julianpeker@hotmail.com');
 
 -- =============================================================================
 -- AMIGOS
@@ -20,18 +23,30 @@ SELECT fn_saveusuario(1, 'julian 01', 'peker 01', 'julianpeker@hotmail.com');
 SELECT fn_saveamigo(1,2);
 -- Cargo un amigo 2
 SELECT fn_saveamigo(1,3);
+-- Cargo un amigo 3
+SELECT fn_saveamigo(2,1);
+-- Cargo un amigo 4
+SELECT fn_saveamigo(3,4);
+-- Cargo un amigo 5
+SELECT fn_saveamigo(3,1);
+-- Cargo un amigo 6
+SELECT fn_saveamigo(4,3);
 -- =============================================================================
 -- FOTO
 -- =============================================================================
 -- Cargo foto 1
 SELECT fn_getidfoto();
-SELECT fn_savefoto(1,'C:\\misdocumentos\fotobariloche.jpg',1,3,'20100120134510');
+SELECT fn_savefoto(1,'fotito.jpg',1,2,'20100120134510');
 -- Cargo foto 2 
 SELECT fn_getidfoto();
-SELECT fn_savefoto(2,'C:\\misdocumentos\fotoasado.jpg',1,3,'20110120134510');
+SELECT fn_savefoto(2,'fin.jpg',1,3,'20110122134510');
 -- Cargo foto 3 
 SELECT fn_getidfoto();
-SELECT fn_savefoto(3,'prueba.jpg',1,2,'20110120134510');
+SELECT fn_savefoto(3,'prueba.jpg',1,2,'20110121134510');
+-- Cargo foto 3 
+SELECT fn_getidfoto();
+SELECT fn_savefoto(4,'tata.jpg',3,4,'20110611134510');
+
 -- Intento ingresar una foto con un usuario sin correo de mail
 --SELECT fn_savefoto(1,'C:\\misdocumentos\fotofiesta.jpg',1,2,'20110120134510');
 
@@ -44,3 +59,18 @@ SELECT fn_savecomentario(1,1,3,'es loco infer');
 -- Cargo com  2 
 SELECT fn_getidcomentario();
 SELECT fn_savecomentario(2,2,3,' que grosso peker');
+-- Cargo com 3
+SELECT fn_getidcomentario();
+SELECT fn_savecomentario(3,1,1,'que noche se viene');
+-- Cargo com  4 
+SELECT fn_getidcomentario();
+SELECT fn_savecomentario(4,2,1,'puede ser');
+-- Cargo com  5 
+SELECT fn_getidcomentario();
+SELECT fn_savecomentario(5,3,4,'tas hermosa gianna hace mucho que no te veo');
+-- Cargo com  6 
+SELECT fn_getidcomentario();
+SELECT fn_savecomentario(7,1,2,'felicidades loco que empieses bien');
+-- Cargo com  7 
+SELECT fn_getidcomentario();
+SELECT fn_savecomentario(8,3,2,'igualmente nos vemos en la vacas jeje ');
--- clases practica/scripts-comentario/04.Delete.sql
+++ clases practica/scripts-comentario/04.Delete.sql
@@ -1,11 +1,22 @@
 -- =============================================================================
 -- Comentarios
 -- =============================================================================
--- Borro una foto 1
+-- Borro una comentario 1
 SELECT pr_deletecomentario(1);
--- Borro una foto 2
+-- Borro una comentario 2
 SELECT pr_deletecomentario(2);
-
+-- Borro una comentario 3
+SELECT pr_deletecomentario(3);
+-- Borro una comentario 4
+SELECT pr_deletecomentario(4);
+-- Borro una comentario 5
+SELECT pr_deletecomentario(5);
+-- Borro una comentario 6
+SELECT pr_deletecomentario(6);
+-- Borro una comentario 5
+SELECT pr_deletecomentario(7);
+-- Borro una comentario 6
+SELECT pr_deletecomentario(8);
 -- =============================================================================
 -- FOTOS
 -- =============================================================================
@@ -13,6 +24,10 @@ SELECT pr_deletecomentario(2);
 SELECT pr_deletefoto(1);
 -- Borro una foto 2
 SELECT pr_deletefoto(2);
+-- Borro una foto 3
+SELECT pr_deletefoto(3);
+-- Borro una foto 4
+SELECT pr_deletefoto(4);
 -- =============================================================================
 -- AMIGOS
 -- =============================================================================
@@ -20,6 +35,14 @@ SELECT pr_deletefoto(2);
 SELECT pr_deleteamigo(1,2); 
 -- Borro un amigo 2
 SELECT pr_deleteamigo(1,3); 
+-- Borro un amigo 2
+SELECT pr_deleteamigo(2,1); 
+-- Cargo un amigo 4
+SELECT pr_deleteamigo(3,4);
+-- Cargo un amigo 5
+SELECT pr_deleteamigo(3,1);
+-- Cargo un amigo 6
+SELECT pr_deleteamigo(4,3);
 -- =============================================================================
 -- Usuarios
 -- =============================================================================
@@ -29,3 +52,5 @@ SELECT  pr_deleteusuario (1);
 SELECT  pr_deleteusuario (2);
 -- Borro un usuario 3
 SELECT  pr_deleteusuario (3);
+-- Borro un usuario 3
+SELECT  pr_deleteusuario (4);
