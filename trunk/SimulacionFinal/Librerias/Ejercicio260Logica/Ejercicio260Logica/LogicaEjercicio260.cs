6fc5f248fb32737b010d8d4686e97eb061d71e00
rodrigo.inf.liberal@gmail.com <rodrigo.inf.liberal@gmail.com@1c33a77f-a4e5-06ae-344c-5a4c18e46920>
2014-02-03 19:20:59 +0000

    metodos estadisticos

--- trunk/SimulacionFinal/Librerias/Ejercicio260Logica/Ejercicio260Logica/LogicaEjercicio260.cs
+++ trunk/SimulacionFinal/Librerias/Ejercicio260Logica/Ejercicio260Logica/LogicaEjercicio260.cs
@@ -20,24 +20,24 @@ namespace Ejercicio260Logica
             new ProblemColumn(new List<object> { "Reloj", 0.0 }, 'd');
         //Columna random llegada paquete
         public ProblemColumn rnd_llp_val =
-            new ProblemColumn(new 
+            new ProblemColumn(new
             List<object> { "Rnd Llegada Paquete", 0.0 }, 'd');
 
         //Columna random procesamiento paquete
         public ProblemColumn rnd_prp_val =
-            new ProblemColumn(new 
+            new ProblemColumn(new
             List<object> { "Rnd Procesamiento Paquete", 0.0 }, 'd');
 
         //Columna evento actual
         public ProblemColumn eventoActual =
-            new ProblemColumn(new 
+            new ProblemColumn(new
             List<object> { "Evento Actual Ejecutandose", "ninguno" }, 's');
 
         //Columna evento siguiente
         public ProblemColumn estadoServidor =
-             new ProblemColumn(new 
-             List<object> { "Estado Selector", "Libre" }, 's');
-        
+             new ProblemColumn(new
+             List<object> { "Estado Selector", "ninguno" }, 's');
+
         //Columna total paquetes simulados
         public ProblemColumn cantidadPaquetesSimulados
             = new ProblemColumn(new
@@ -47,6 +47,16 @@ namespace Ejercicio260Logica
         //Todos los paquetes vienen con el valor en falso descartado
         //Es decir no descartado.
         //Si el paquete es descartado se lo setea para contar los descartados
+
+        //Columna total paquetes informacion procesados
+        public ProblemColumn cantidadPaquetesProcesados
+            = new ProblemColumn(new
+            List<object> { "Total Paquetes Procesados", 0 }, 'i');
+
+        //Columna total paquetes informacion descartados
+        public ProblemColumn cantidadPaquetesDescartados
+            = new ProblemColumn(new
+            List<object> { "Total Paquetes Descardados", 0 }, 'i');
 
         //Cola de paquetes que se van almacenando en la memoria
         //y se van sacando para ser procesados
@@ -58,20 +68,10 @@ namespace Ejercicio260Logica
         public List<PaqueteInformacion> colaPaqueteInformacionDescartados =
            new List<PaqueteInformacion>();
 
-        //Columna total paquetes informacion descartados
-        public ProblemColumn cantidadPaquetesDescartados
-            = new ProblemColumn(new 
-            List<object> { "Total Paquetes Descardados", 0 }, 'i');
-
         //Cola de procesados para contar la cantidad de paquetes procesados
         //y en funcion de eso obtener el nivel de confianza
         public List<PaqueteInformacion> colaPaqueteInformacionProcesados =
            new List<PaqueteInformacion>();
-
-        //Columna total paquetes informacion procesados
-        public ProblemColumn cantidadPaquetesProcesados
-            = new ProblemColumn(new 
-            List<object> { "Total Paquetes Procesados", 0 }, 'i');
 
         #endregion relojYCola
 
@@ -92,7 +92,7 @@ namespace Ejercicio260Logica
 
         // Llegada paquete
         public Event llegadaPaquete =
-        new Event(new 
+        new Event(new
             List<object> { "Llegada Paquete", 0.0, 0.0, 0.0, "Libre" });
         private CStrategy_NegExpDist
             rnd_llp = new CStrategy_NegExpDist(0.48);
@@ -101,13 +101,20 @@ namespace Ejercicio260Logica
 
         // Procesamiento paquete
         public Event procesamientoPaquete =
-        new Event(new 
+        new Event(new
             List<object> { "Procesamiento Paquete", 0.0, 0.0, 0.0, "Libre" });
         private CStrategy_NegExpDist
-            rnd_prp = new CStrategy_NegExpDist(0.38);
+            rnd_prp = new CStrategy_NegExpDist(0.75);
 
         // Cada paquete se procesa cada 0.002 segundos
         #endregion distribucionesDeProbabilidad
+        #region Estadisticas
+        public List<double> listaDeMediasPorSimulacion = new List<double>();
+        public double valorMediaTotal = 0;
+        public double valorVarianzaTotal = 0;
+        public int cantidadPaquetesASimularPorSimulacion = 0;
+        public int nroDeSimulacionesARealizar = 0;
+        #endregion Estadisticas
         #endregion Atributos
 
         #region Metodos
@@ -148,7 +155,7 @@ namespace Ejercicio260Logica
             if (
                 (double)llegadaPaquete.getObjectListEvent(2) <
                 (double)procesamientoPaquete.getObjectListEvent(2)
-                || 
+                ||
                 estadoServidor.
                 getObjectProblemColumn(1).ToString().Equals("Libre")
                 )
@@ -160,7 +167,7 @@ namespace Ejercicio260Logica
                     setObjectProblemColumn
                     (1, (double)llegadaPaquete.getObjectListEvent(2));
             }
-            else 
+            else
             {
                 //Ejecuto procesamiento de paquete como proximo evento
                 reloj_anterior.setObjectProblemColumn(1,
@@ -178,7 +185,7 @@ namespace Ejercicio260Logica
         private void contarTotalPaquetes()
         {
             cantidadPaquetesSimulados.setObjectProblemColumn(1,
-                colaPaqueteInformacionProcesados.Count 
+                colaPaqueteInformacionProcesados.Count
                 + colaPaqueteInformacionDescartados.Count);
         }
 
@@ -206,7 +213,7 @@ namespace Ejercicio260Logica
                 limiteColaPaquetesInformacion)
             {
                 //Si la cola no esta llena añado el paquete
-               // colaPaquetesASerProcesados.Add(paqueteInformacion);
+                // colaPaquetesASerProcesados.Add(paqueteInformacion);
                 // hacer validacion cuando se llena la cola
             }
             else
@@ -254,61 +261,125 @@ namespace Ejercicio260Logica
                     //colaPaquetesASerProcesados.RemoveAt(0);
                     dispararProcesamientoDePaquete();
                 }
-                else 
+                else
                 {
                     estadoServidor.setObjectProblemColumn(1, "Libre");
                     //Nuevalinea
                     //dispararLlegadaDePaquete();
                 }
-                
+
                 return;
             }
         }
         #endregion ProcesamientoDeEventoActual
 
         #region BucleDeEjecucionDeSimulacion
-        public void execute_logic(double initTime = 0.0, double endTime = 0.0, int tamaniodeCola = 0)
+        public void execute_logic(double initTime = 0.0,
+            double endTime = 0.0, int tamanioDeCola = 0)
         {
+            int cantidadSimulaciones = 10;
+            cantidadPaquetesASimularPorSimulacion = 100;
             reloj.setObjectProblemColumn(1, initTime);
             Debug.WriteLine("Inicio");
             //Seteo el limite de la cola
             limiteColaPaquetesInformacion = 1;
-            while ( (int) cantidadPaquetesSimulados
-                    .getObjectProblemColumn(1) < 1000
-                    )
+            //colaPaqueteInformacionProcesados.Count
+            while (nroDeSimulacionesARealizar < cantidadSimulaciones)
             {
-                if (secondRow == false)
+                Debug.WriteLine("Nro de Simulacion " + nroDeSimulacionesARealizar);
+                nroDeSimulacionesARealizar++;
+                iniciarNuevaSimulacion();
+                estadoServidor.setObjectProblemColumn(1, "Libre");
+                while ((int)cantidadPaquetesSimulados.getObjectProblemColumn(1)
+                    < cantidadPaquetesASimularPorSimulacion
+                    )
                 {
-                    //Primera Fila.
-                    Debug.WriteLine("Primera Fila");
-                    //Disparo llegada de paquete
-                    dispararLlegadaDePaquete();
-                    //Muestro salidas
-                    //debugOutputs();
-                    //Seteo inicio de procesamiento en 0 
-                    procesamientoPaquete.setObjectListEvent(2, (double)0);
-                    //Seteo el reloj
-                    reloj_anterior.setObjectProblemColumn(1, 
-                        (double) reloj.getObjectProblemColumn(1));
-                    reloj.setObjectProblemColumn
-                      (1, llegadaPaquete.getObjectListEvent(2));
-                    //De ahora en mas todo el bucle se limita a next event
-                    secondRow = true;
+                    if (secondRow == false)
+                    {
+                        //Primera Fila.
+                        Debug.WriteLine("Primera Fila");
+                        //Disparo llegada de paquete
+                        dispararLlegadaDePaquete();
+                        //Muestro salidas
+                        //debugOutputs();
+                        //Seteo inicio de procesamiento en 0 
+                        procesamientoPaquete.setObjectListEvent(2, (double)0);
+                        //Seteo el reloj
+                        reloj_anterior.setObjectProblemColumn(1,
+                            (double)reloj.getObjectProblemColumn(1));
+                        reloj.setObjectProblemColumn
+                          (1, llegadaPaquete.getObjectListEvent(2));
+                        //De ahora en mas todo el bucle se limita a next event
+                        secondRow = true;
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Segunda Fila");
+                        // evento a ejecutar ahora
+                        executeCurrentEvent();
+                        // Averiguar proximo evento
+                        siguienteEvento();
+                    }
+                    //Cuento la cantidad de paquetes (descartados + procesados)
+                    contarTotalPaquetes();
+                    debugOutputs();
+
                 }
-                else
-                {
-                    Debug.WriteLine("Segunda Fila");
-                    // evento a ejecutar ahora
-                    executeCurrentEvent();
-                    // Averiguar proximo evento
-                    siguienteEvento();
-                }
-                //Cuento la cantidad de paquetes (descartados + procesados)
-                contarTotalPaquetes();
-                debugOutputs();
+                listaDeMediasPorSimulacion.Add(
+                (double)((double)colaPaqueteInformacionDescartados.Count /
+                (double)((int)cantidadPaquetesSimulados.getObjectProblemColumn(1))));
+                Debug.WriteLine(
+                "Valor de media obtenida por simulacion = "
+                + listaDeMediasPorSimulacion[listaDeMediasPorSimulacion.Count - 1]);
             }
+            valorMediaTotal = listaDeMediasPorSimulacion.Average();
+            valorVarianzaTotal = calcularVarianzaTotal();
+            Debug.WriteLine(
+                "Valor de la media total = "
+                + listaDeMediasPorSimulacion.Average());
+            Debug.WriteLine(
+                "Valor de la varianza total = "
+                + valorVarianzaTotal);
         }
         #endregion BucleDeEjecucionDeSimulacion
+
+        #region BorrarListas
+        private void iniciarNuevaSimulacion()
+        {
+            colaPaquetesASerProcesados.Clear();
+            colaPaqueteInformacionDescartados.Clear();
+            colaPaqueteInformacionProcesados.Clear();
+            cantidadPaquetesSimulados.restartProblemColumm('i');
+            cantidadPaquetesProcesados.restartProblemColumm('i');
+            cantidadPaquetesDescartados.restartProblemColumm('i');
+            reloj.restartProblemColumm('d');
+            reloj_anterior.restartProblemColumm('d');
+            rnd_llp_val.restartProblemColumm('d');
+            rnd_prp_val.restartProblemColumm('d');
+            eventoActual.restartProblemColumm('s');
+            estadoServidor.restartProblemColumm('s');
+            llegadaPaquete.restartEvent(new
+            List<object> { 0.0, 0.0, 0.0 });
+            procesamientoPaquete.restartEvent(new
+            List<object> { 0.0, 0.0, 0.0 });
+        }
+
+        #endregion BorrarListas
+
+        #region MetodosEstadisticos
+        private double calcularVarianzaTotal()
+        {
+            double _valorVarianzaTotal = 0;
+            foreach (double media in listaDeMediasPorSimulacion) 
+            {
+                _valorVarianzaTotal = _valorVarianzaTotal + 
+                Math.Pow((media - 
+                listaDeMediasPorSimulacion.Average()),2);
+            }
+            _valorVarianzaTotal = _valorVarianzaTotal / (listaDeMediasPorSimulacion.Count - 1);
+            return  _valorVarianzaTotal;
+        }
+        #endregion MetodosEstadisticos
 
         #region MetodosDeDebug
         private void debugOutputs()
