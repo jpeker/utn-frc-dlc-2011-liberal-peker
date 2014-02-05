86cf6acf79413ad5af73fc08689a53529aab54e5
rodrigo.inf.liberal <rodrigo.inf.liberal@gmail.com>
2014-02-05 01:02:43 +0000

    Falta terminar gui y mostrar estadisticas y completar logica

---
+++ SimulacionFinal/Librerias/Ejercicio260Logica/Ejercicio260Logica/GradosDeLibertad.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio260Logica
+{
+    public static class GradosDeLibertad
+    {
+        public static List<double> valorGradosLibertad = new List<double>();
+
+        public static void llenarListaGradosLibertad()
+        {
+            valorGradosLibertad.Add(6.635);
+            valorGradosLibertad.Add(9.210);
+            valorGradosLibertad.Add(11.345);
+            valorGradosLibertad.Add(13.277);
+            valorGradosLibertad.Add(15.086);
+            valorGradosLibertad.Add(16.812);
+            valorGradosLibertad.Add(18.475);
+            valorGradosLibertad.Add(20.090);
+            valorGradosLibertad.Add(21.666);
+            valorGradosLibertad.Add(23.209);
+            valorGradosLibertad.Add(24.725);
+            valorGradosLibertad.Add(26.217);
+            valorGradosLibertad.Add(27.688);
+            valorGradosLibertad.Add(29.141);
+            valorGradosLibertad.Add(30.578);
+            valorGradosLibertad.Add(32.000);
+            valorGradosLibertad.Add(33.409);
+            valorGradosLibertad.Add(34.805);
+            valorGradosLibertad.Add(36.191);
+        }
+    }
+}
