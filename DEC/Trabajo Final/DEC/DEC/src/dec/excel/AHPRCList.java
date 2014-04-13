21fc76d948da876805a08832b2cd7f120378398d
rodrigo.inf.liberal <rodrigo.inf.liberal@gmail.com>
2014-04-13 00:23:10 +0000

    gui/NuevoProblema
    AHPRCList.java 
    
    añadidos y modificados.

---
+++ DEC/Trabajo Final/DEC/DEC/src/dec/excel/AHPRCList.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dec.excel;
+
+import java.util.Map;
+
+/**
+ *
+ * @author PC ACER
+ */
+public class AHPRCList {
+    public Map<Integer, Double> mapaValoresAlternativas;
+    public AHPRCList(){
+
+    }
+    //public double getMapValue(int cantAlternativas){
+    //double
+   // }
+
+}
