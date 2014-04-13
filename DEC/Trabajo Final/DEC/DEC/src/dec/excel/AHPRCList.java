3a320f394a0a7fdf1fff7be130e90e8b2aaf76f0
rodrigo.inf.liberal <rodrigo.inf.liberal@gmail.com>
2014-04-13 00:38:39 +0000

    AHPRCList modified.

--- DEC/Trabajo Final/DEC/DEC/src/dec/excel/AHPRCList.java
+++ DEC/Trabajo Final/DEC/DEC/src/dec/excel/AHPRCList.java
@@ -14,10 +14,22 @@ import java.util.Map;
 public class AHPRCList {
     public Map<Integer, Double> mapaValoresAlternativas;
     public AHPRCList(){
-
+    mapaValoresAlternativas.put(3, 0.58);
+    mapaValoresAlternativas.put(4, 0.9);
+    mapaValoresAlternativas.put(5, 1.12);
+    mapaValoresAlternativas.put(6, 1.24);
+    mapaValoresAlternativas.put(7, 1.32);
+    mapaValoresAlternativas.put(8, 1.41);
+    mapaValoresAlternativas.put(9, 1.45);
+    mapaValoresAlternativas.put(10, 1.49);
     }
-    //public double getMapValue(int cantAlternativas){
-    //double
-   // }
+    public double getMapValue(int cantAlternativas){
+    double mapValue = -1;
+    if(cantAlternativas>2)
+    {
+        mapValue = mapaValoresAlternativas.get(cantAlternativas);
+    }
+    return mapValue;
+    }
 
 }
